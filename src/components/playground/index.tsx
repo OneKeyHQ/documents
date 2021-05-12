93d91a94a0e332f184df98ef3d5ab0cced7e8e95
Arslan Ablikim <arslan.ablikim0@gmail.com>
2021-05-12 11:58:45 +0800

    playground

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,19 +1,30 @@
-import React, { useEffect, useReducer, useState } from 'react';
-import MonacoEditor from 'react-monaco-editor';
-import { MonacoEditorProps } from "react-monaco-editor/lib/types";
+import React, { useEffect, useReducer, useRef, useState } from 'react';
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import OneKeyConnect from "@onekeyhq/connect";
 import { loggerReducer } from "./loggerReducer";
+import styles from './index.module.css';
 
-interface PlayroundProps extends MonacoEditorProps {
+interface PlaygroundProps {
     initValue?: string;
 }
 
-function Playground(props: PlayroundProps) {
+function Playground(props: PlaygroundProps) {
     const [value, setValue] = useState(props.initValue ?? '');
     const [log, setLog] = useReducer(loggerReducer, '');
-
+    const editorRef = useRef<HTMLDivElement>();
 
     useEffect(() => {
+        //monaco
+        monaco.languages.typescript.javascriptDefaults.addExtraLib(`
+            declare global {
+                interface Window { OneKeyConnect: {}; }
+            }`);
+        monaco.editor.create(editorRef.current, {
+            value: props.initValue,
+            language: 'typescript'
+        });
+
+        // OneKeyConnect
         OneKeyConnect.on('UI_EVENT', event => {
             console.log('UI_EVENT', event);
         });
@@ -38,11 +49,9 @@ function Playground(props: PlayroundProps) {
     }
     return (
         <>
-
-            <MonacoEditor
-                value={value}
-                onChange={setValue}
-                {...props}
+            <div
+                className={styles.editor}
+                ref={editorRef}
             />
             <button onClick={run}>RUN</button>
             <div>{log}</div>
